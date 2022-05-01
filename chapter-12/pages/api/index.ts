bb7596e700222819e0a00e056a60bb7ebe1d5173
Michael Hayes <michael@hayes.io>
2022-04-30 23:32:36 -0700

    Example of pothos version

--- chapter-12/pages/api/index.ts
+++ chapter-12/pages/api/index.ts
@@ -1,15 +1,14 @@
-import { createServer, GraphQLYogaError } from "@graphql-yoga/node";
-import { join } from "path";
-import { readFileSync } from "fs";
-import currencyFormatter from "currency-formatter";
-import type { PrismaClient } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { createServer, GraphQLYogaError } from '@graphql-yoga/node';
+import type { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import currencyFormatter from 'currency-formatter';
+import SchemaBuilder from '@pothos/core';
+import PrismaPlugin from '@pothos/plugin-prisma';
+import type PrismaTypes from '@pothos/plugin-prisma/generated';
 
-import prisma from "../../lib/prisma";
-import { Resolvers } from "../../types";
-import { findOrCreateCart } from "../../lib/cart";
-import { stripe } from "../../lib/stripe";
-import { origin } from "../../lib/client";
+import prisma from '../../lib/prisma';
+import { stripe } from '../../lib/stripe';
+import { origin } from '../../lib/client';
 
 export type GraphQLContext = {
   prisma: PrismaClient;
@@ -21,112 +20,287 @@ export async function createContext(): Promise<GraphQLContext> {
   };
 }
 
-const currencyCode = "USD";
+const currencyCode = 'USD';
 
-const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
-  encoding: "utf-8",
+const builder = new SchemaBuilder<{
+  Context: GraphQLContext;
+  PrismaTypes: PrismaTypes;
+  Scalars: {
+    ID: { Input: string; Output: string };
+  };
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
 });
 
-const resolvers: Resolvers = {
-  Query: {
-    cart: async (_, { id }, { prisma }) => {
-      return findOrCreateCart(prisma, id);
+builder.queryType({
+  fields: (t) => ({
+    cart: t.prismaField({
+      type: 'Cart',
+      args: {
+        id: t.arg.id({ required: true }),
+      },
+      resolve: (query, _, { id }) =>
+        prisma.cart.upsert({
+          ...query,
+          where: { id },
+          update: {},
+          create: {
+            id,
+          },
+        }),
+    }),
+  }),
+});
+
+const Money = builder.objectRef<number>('Money').implement({
+  fields: (t) => ({
+    amount: t.int({ resolve: (amount) => amount }),
+    formatted: t.string({
+      resolve: (amount) =>
+        currencyFormatter.format(amount / 100, {
+          code: currencyCode,
+        }),
+    }),
+  }),
+});
+
+builder.prismaObject('Cart', {
+  findUnique: ({ id }) => ({ id }),
+  fields: (t) => ({
+    id: t.exposeID('id', {}),
+    totalItems: t.int({
+      select: {
+        items: true,
+      },
+      resolve: (cart) =>
+        cart.items.reduce((total, item) => total + item.quantity || 1, 0),
+    }),
+    items: t.relation('items'),
+    subTotal: t.field({
+      select: {
+        items: true,
+      },
+      type: Money,
+      resolve: (cart) =>
+        cart.items.reduce((acc, item) => acc + item.price * item.quantity, 0) ??
+        0,
+    }),
+  }),
+});
+
+builder.prismaObject('CartItem', {
+  findUnique: ({ id, cartId }) => ({ id_cartId: { id, cartId } }),
+  fields: (t) => ({
+    id: t.exposeID('id'),
+    name: t.exposeString('name'),
+    description: t.exposeString('description', { nullable: true }),
+    quantity: t.exposeInt('quantity'),
+    image: t.exposeString('image', { nullable: true }),
+    unitTotal: t.expose('price', { type: Money }),
+    lineTotal: t.field({
+      type: Money,
+      resolve: (item) => item.price * item.quantity,
+    }),
+  }),
+});
+
+builder.mutationType({});
+
+const AddToCartInput = builder.inputType('AddToCartInput', {
+  fields: (t) => ({
+    cartId: t.id({ required: true }),
+    id: t.id({ required: true }),
+    name: t.string({ required: true }),
+    description: t.string(),
+    image: t.string(),
+    price: t.int({ required: true }),
+    quantity: t.int({ defaultValue: 1 }),
+  }),
+});
+
+builder.mutationField('addItem', (t) =>
+  t.prismaField({
+    type: 'Cart',
+    args: {
+      input: t.arg({ required: true, type: AddToCartInput }),
     },
-  },
-  Mutation: {
-    addItem: async (_, { input }, { prisma }) => {
-      const cart = await findOrCreateCart(prisma, input.cartId);
-      await prisma.cartItem.upsert({
+    resolve: (query, _, { input }) => {
+      const item = {
+        id: input.id,
+        name: input.name,
+        description: input.description,
+        image: input.image,
+        price: input.price,
+        quantity: input.quantity ?? 1,
+      };
+
+      return prisma.cart.upsert({
+        ...query,
+        where: { id: input.cartId },
         create: {
-          cartId: cart.id,
-          id: input.id,
-          name: input.name,
-          description: input.description,
-          image: input.image,
-          price: input.price,
-          quantity: input.quantity || 1,
+          id: input.cartId,
+          items: {
+            create: item,
+          },
         },
-        where: { id_cartId: { id: input.id, cartId: cart.id } },
         update: {
-          quantity: {
-            increment: input.quantity || 1,
-          },
-        },
-      });
-      return cart;
-    },
-    removeItem: async (_, { input }, { prisma }) => {
-      const { cartId } = await prisma.cartItem.delete({
-        where: { id_cartId: { id: input.id, cartId: input.cartId } },
-        select: {
-          cartId: true,
-        },
-      });
-      return findOrCreateCart(prisma, cartId);
-    },
-    increaseCartItem: async (_, { input }, { prisma }) => {
-      const { cartId, quantity } = await prisma.cartItem.update({
-        data: {
-          quantity: {
-            increment: 1,
-          },
-        },
-        where: { id_cartId: { id: input.id, cartId: input.cartId } },
-        select: {
-          quantity: true,
-          cartId: true,
-        },
-      });
-      return findOrCreateCart(prisma, cartId);
-    },
-    decreaseCartItem: async (_, { input }, { prisma }) => {
-      const { cartId, quantity } = await prisma.cartItem.update({
-        data: {
-          quantity: {
-            decrement: 1,
-          },
-        },
-        where: { id_cartId: { id: input.id, cartId: input.cartId } },
-        select: {
-          quantity: true,
-          cartId: true,
-        },
-      });
-      // Allow users to decrease any number of times
-      if (quantity <= 0) {
-        await prisma.cartItem.update({
-          where: { id_cartId: { id: input.id, cartId: input.cartId } },
-          data: {
-            quantity: {
-              set: 0,
+          items: {
+            upsert: {
+              create: item,
+              where: { id_cartId: { id: input.id, cartId: input.cartId } },
+              update: {
+                quantity: {
+                  increment: input.quantity ?? 1,
+                },
+              },
             },
           },
-        });
-      }
-      return findOrCreateCart(prisma, cartId);
+        },
+      });
     },
-    createCheckoutSession: async (_, { input }, { prisma }) => {
+  })
+);
+
+const RemoveFromCartInput = builder.inputType('RemoveFromCartInput', {
+  fields: (t) => ({
+    cartId: t.id({ required: true }),
+    id: t.id({ required: true }),
+  }),
+});
+
+builder.mutationField('removeItem', (t) =>
+  t.prismaField({
+    type: 'Cart',
+    args: {
+      input: t.arg({ type: RemoveFromCartInput, required: true }),
+    },
+    resolve: (query, _, { input }) =>
+      prisma.cart.update({
+        ...query,
+        where: { id: input.id },
+        data: {
+          items: {
+            deleteMany: {
+              id: input.id,
+            },
+          },
+        },
+      }),
+  })
+);
+
+const IncreaseCartItemInput = builder.inputType('IncreaseCartItemInput', {
+  fields: (t) => ({
+    cartId: t.id({ required: true }),
+    id: t.id({ required: true }),
+  }),
+});
+
+builder.mutationField('increaseCartItem', (t) =>
+  t.prismaField({
+    type: 'Cart',
+    args: {
+      input: t.arg({ type: IncreaseCartItemInput, required: true }),
+    },
+    resolve: (query, _, { input }) =>
+      prisma.cart.update({
+        ...query,
+        where: { id: input.id },
+        data: {
+          items: {
+            updateMany: {
+              where: { id: input.id },
+              data: {
+                quantity: {
+                  increment: 1,
+                },
+              },
+            },
+          },
+        },
+      }),
+  })
+);
+
+const DecreaseCartItemInput = builder.inputType('DecreaseCartItemInput', {
+  fields: (t) => ({
+    cartId: t.id({ required: true }),
+    id: t.id({ required: true }),
+  }),
+});
+
+builder.mutationField('decreaseCartItem', (t) =>
+  t.prismaField({
+    type: 'Cart',
+    args: {
+      input: t.arg({ type: DecreaseCartItemInput, required: true }),
+    },
+    resolve: (query, _, { input }) =>
+      prisma.cart.update({
+        ...query,
+        where: { id: input.id },
+        data: {
+          items: {
+            updateMany: {
+              where: { id: input.id, quantity: { gte: 1 } },
+              data: {
+                quantity: {
+                  decrement: 1,
+                },
+              },
+            },
+          },
+        },
+      }),
+  })
+);
+
+const CreateCheckoutSessionInput = builder.inputType(
+  'CreateCheckoutSessionInput',
+  {
+    fields: (t) => ({
+      cartId: t.id({ required: true }),
+    }),
+  }
+);
+
+const CheckoutSession = builder
+  .objectRef<{ id: string; url: string | null }>('CheckoutSession')
+  .implement({
+    fields: (t) => ({
+      id: t.exposeID('id'),
+      url: t.exposeID('url', { nullable: true }),
+    }),
+  });
+
+builder.mutationField('createCheckoutSession', (t) =>
+  t.field({
+    type: CheckoutSession,
+    args: {
+      input: t.arg({ type: CreateCheckoutSessionInput, required: true }),
+    },
+    resolve: async (_, { input }) => {
       const { cartId } = input;
 
       const cart = await prisma.cart.findUnique({
         where: { id: cartId },
+        include: {
+          items: true,
+        },
       });
 
       if (!cart) {
-        throw new GraphQLYogaError("Invalid cart");
+        throw new GraphQLYogaError('Invalid cart');
       }
 
-      const cartItems = await prisma.cart
-        .findUnique({
-          where: { id: cartId },
-        })
-        .items();
-
-      if (!cartItems || cartItems.length === 0) {
-        throw new GraphQLYogaError("Cart is empty");
+      if (!cart.items || cart.items.length === 0) {
+        throw new GraphQLYogaError('Cart is empty');
       }
 
-      const line_items = cartItems.map((item) => {
+      const line_items = cart.items.map((item) => {
         return {
           quantity: item.quantity,
           price_data: {
@@ -148,7 +322,7 @@ const resolvers: Resolvers = {
         metadata: {
           cartId: cart.id,
         },
-        mode: "payment",
+        mode: 'payment',
       });
 
       return {
@@ -156,73 +330,15 @@ const resolvers: Resolvers = {
         url: session.url,
       };
     },
-  },
-  Cart: {
-    items: async ({ id }, _, { prisma }) => {
-      // https://www.prisma.io/docs/guides/performance-and-optimization/query-optimization-performance#solving-n1-in-graphql-with-findunique-and-prismas-dataloader
-      const items = await prisma.cart
-        .findUnique({
-          where: { id },
-        })
-        .items();
-      return items;
-    },
-    totalItems: async ({ id }, _, { prisma }) => {
-      const items = await prisma.cart
-        .findUnique({
-          where: { id },
-        })
-        .items();
-      return items.reduce((total, item) => total + item.quantity || 1, 0);
-    },
-    subTotal: async ({ id }, _, { prisma }) => {
-      const items = await prisma.cart
-        .findUnique({
-          where: { id },
-        })
-        .items();
-      const amount =
-        items.reduce((acc, item) => acc + item.price * item.quantity, 0) ?? 0;
-      return {
-        amount,
-        formatted: currencyFormatter.format(amount / 100, {
-          code: currencyCode,
-        }),
-      };
-    },
-  },
-  CartItem: {
-    unitTotal: (item) => {
-      const amount = item.price;
-      return {
-        amount,
-        formatted: currencyFormatter.format(amount / 100, {
-          code: currencyCode,
-        }),
-      };
-    },
-    lineTotal: (item) => {
-      const amount = item.quantity * item.price;
-
-      return {
-        amount,
-        formatted: currencyFormatter.format(amount / 100, {
-          code: currencyCode,
-        }),
-      };
-    },
-  },
-};
+  })
+);
 
 const server = createServer<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({
-  endpoint: "/api",
-  schema: {
-    typeDefs,
-    resolvers,
-  },
+  endpoint: '/api',
+  schema: builder.toSchema({}),
   context: createContext(),
 });
 
